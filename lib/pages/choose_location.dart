5715bc4db0ee10d88d694353d86caf0003c8f84f
s21kkurt <kkurt@sabanciuniv.edu>
2021-03-24 14:35:26 +0300

    Flutter Tutorial for Beginners #25 - Asynchronous Code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,17 +6,30 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  void getData() async {
+    //simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'kerem';
+    });
+
+    //simulate network request for a bio
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'student';
+    });
+
+    print('$username - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print("initState function ran");
+    //print("initState function ran");
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build function ran");
+    //print("build function ran");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
