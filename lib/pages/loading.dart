33c0c454f0bfba1adae5871873f2c735c0d15895
s21kkurt <kkurt@sabanciuniv.edu>
2021-03-25 20:58:48 +0300

    Flutter Tutorial for Beginners #28 - WorldTime Custom Class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:worldtimeapp/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,36 +7,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    //make http request
-    Response response =
-        await get('http://worldtimeapi.org/api/timezone/America/Los_Angeles');
-    Map data = jsonDecode(response.body);
-    //print(data);
+  String time = 'loading';
 
-    //get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    //print(datetime);
-    //print(offset);
-
-    //Create datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
     //print("initState function ran");
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Loading Screen"),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
