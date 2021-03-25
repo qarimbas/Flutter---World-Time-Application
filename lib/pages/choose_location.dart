dc50bb3e4e3d86770271dfd64556741dbadd7c78
s21kkurt <kkurt@sabanciuniv.edu>
2021-03-25 23:22:40 +0300

    Final Build

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,26 +6,41 @@ class ChooseLocation extends StatefulWidget {
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
-List<WorldTime> locations = [
-  WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
-  WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
-  WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
-  WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
-  WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
-  WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
-  WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
-  WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
-];
-
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //print("build function ran");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text("Choose a Location"),
+        title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
       ),
@@ -37,7 +52,9 @@ class _ChooseLocationState extends State<ChooseLocation> {
                   const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
               child: Card(
                 child: ListTile(
-                  onTap: () {},
+                  onTap: () {
+                    updateTime(index);
+                  },
                   title: Text(locations[index].location),
                   leading: CircleAvatar(
                     backgroundImage:
